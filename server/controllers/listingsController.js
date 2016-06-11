5e3f9b88ec0983089a6968f030321b4a649b18ec
aaronwbrown <aaron.w.brown13@gmail.com>
2016-06-11 11:07:59 -0700

    Filter was working before merge. Now getting a map image error. Moving into debugging to track/isolate bug.

--- server/controllers/listingsController.js
+++ server/controllers/listingsController.js
@@ -33,7 +33,7 @@ exports.getAll = function(category, callback) {
 // Controller method - get filtered results
 exports.getFiltered = function(filters, callback) {
   // constructing where object body
-  console.log('filters: ', filters);
+  console.log('filters', filters);
   let filteredWhere = {};
   if (Object.keys(filters).length <= 1) {
     filteredWhere = {};
@@ -57,9 +57,10 @@ exports.getFiltered = function(filters, callback) {
       }
       filteredWhere.distance = distance;
     } else {
-      filteredWhere.distance = {};
+        delete filteredWhere.distance;
     }
   }
+  console.log('filteredWhere: ', filteredWhere);
   db.Listing.findAll({
     include:
     [{
@@ -86,7 +87,6 @@ exports.getFiltered = function(filters, callback) {
       console.error(error);
       callback(404, error);
     });
-  console.log('fW', filteredWhere);
 };
 
 //Controller method - add a listings to DB
@@ -104,14 +104,14 @@ exports.addOne = function(listing, images, callback) {
               console.log('Image upload successful');
             })
             .catch(function(error) {
-              console.error('image upload error', error);
+              console.error(error);
             });
         });
       }
       callback(201, listing);
     })
     .catch(function(error) {
-      console.error('posting new one error', error);
+      console.error(error);
       callback(404, error);
     });
 };
